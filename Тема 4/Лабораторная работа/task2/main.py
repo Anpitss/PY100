e4928b774502a663c9e75def9f9cac39be34979a
Angelina <mosunova.as@edu.spbstu.ru>
2023-04-06 02:12:04 +0300

    Lab_3

--- Тема 4/Лабораторная работа/task2/main.py
+++ Тема 4/Лабораторная работа/task2/main.py
@@ -1,5 +1,23 @@
 def get_count_char(str_):
-    ...  # TODO посчитать количество каждой буквы в строке в аргументе str_
+    dict_ = {}
+    str_low = str_.lower()
+    for symbol_ in str_low:
+        if symbol_.isalpha():
+            if symbol_ in dict_:
+                dict_[symbol_] += 1
+            else:
+                dict_[symbol_] = 1
+    return dict_
+
+def get_percent_char(dict_):
+    sum_dict = 0
+    for symbol_ in dict_:
+        sum_dict += dict_[symbol_]
+    for symbol_ in dict_:
+        dict_[symbol_] = round(dict_[symbol_] / sum_dict * 100)
+    return dict_
+
+
 
 
 main_str = """
@@ -8,3 +26,4 @@ main_str = """
     Далее нужно отсортировать слова в алфавитном порядке, а после сортировки склеить их с помощью метода строк join. Приступим!!!!
 """
 print(get_count_char(main_str))
+print(get_percent_char(get_count_char(main_str)))
--- Тема 4/Лабораторная работа/task3/main.py
+++ Тема 4/Лабораторная работа/task3/main.py
@@ -1,6 +1,10 @@
 def delete(list_, index=None):
     ...  # TODO реализовать функцию удаления элемента из списка по индексу
-
+    if index == -1 or index is None:
+        return list_[0:(len(list_) - 1)]
+    else:
+        list_ = list_[0:index] + list_[(index + 1):len(list_)]
+        return list_
 
 print(delete([0, 0, 1, 2], index=0))  # [0, 1]
 print(delete([0, 1, 1, 2, 3], index=1))  # [0, 1, 2]
