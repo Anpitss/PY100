0810ce224168d6b4b71b98b3dc027a96a927bd8a
Angelina <mosunova.as@edu.spbstu.ru>
2023-04-06 18:36:04 +0300

    Lab_4

--- Тема 5/Лабораторная работа/task3/main.py
+++ Тема 5/Лабораторная работа/task3/main.py
@@ -1,6 +1,12 @@
+import random
 def get_unique_list_numbers() -> list[int]:
     ...  # TODO написать функцию для получения списка уникальных целых чисел
-
+    random_list = [random.randint(-10, 10)]
+    while len(random_list) < 15:
+        random_l = random.randint(-10, 10)
+        if random_l not in random_list:
+            random_list.append(random_l)
+    return random_list
 
 list_unique_numbers = get_unique_list_numbers()
 print(list_unique_numbers)
